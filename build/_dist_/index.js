2a1764320e28a12d5f86f3063968babf227906cc
Shimon Wosner <SWosner@OKGTech.Local>
2020-05-21 12:32:23 -0400

    d

--- build/_dist_/index.js
+++
@@ -1,5 +0,0 @@
-import React from "/web_modules/react.js";
-import ReactDOM from "/web_modules/react-dom.js";
-import App2 from "./App.js";
-import "./index.css.proxy.js";
-ReactDOM.render(React.createElement(React.StrictMode, null, React.createElement(App2, null)), document.getElementById("root"));
