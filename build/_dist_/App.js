b77f1b0470d75e49e645b8bbac4db3392b9a7639
Shimon Wosner <SWosner@OKGTech.Local>
2020-05-21 12:39:16 -0400

    s

---
+++ build/_dist_/App.js
@@ -0,0 +1,9 @@
+import React from "/web_modules/react.js";
+import logo2 from "./logo.svg.proxy.js";
+import "./App.css.proxy.js";
+function App2() {
+  return React.createElement("div", {
+    className: "App"
+  }, "test");
+}
+export default App2;
