a2fa775f8b253bb8b6b82c2b9d256ddff8c3abc4
Shimon Wosner <SWosner@OKGTech.Local>
2020-05-22 11:36:35 -0400

    t

--- build/_dist_/App.js
+++ build/_dist_/App.js
@@ -1,6 +1,6 @@
 import React from "/web_modules/react.js";
-import logo2 from "./logo.svg.proxy.js";
-import "./App.css.proxy.js";
+import logo2 from "./logo.svg";
+import "./App.css";
 function App2() {
   return React.createElement("div", {
     className: "App"
--- build/_dist_/index.js
+++ build/_dist_/index.js
@@ -1,5 +1,5 @@
 import React from "/web_modules/react.js";
 import ReactDOM from "/web_modules/react-dom.js";
 import App2 from "./App.js";
-import "./index.css.proxy.js";
+import "./index.css";
 ReactDOM.render(React.createElement(React.StrictMode, null, React.createElement(App2, null)), document.getElementById("root"));
