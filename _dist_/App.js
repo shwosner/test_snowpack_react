f19d751a003db8151559c28b86aad38401736dbd
Shimon Wosner <SWosner@OKGTech.Local>
2020-05-22 12:26:58 -0400

    c

--- _dist_/App.js
+++ _dist_/App.js
@@ -3,6 +3,6 @@ import "./App.css";
 function App2() {
   return React.createElement("div", {
     className: "App"
-  }, "test12355", React.createElement("h1", null, "tes heading"), React.createElement("ul", null, React.createElement("li", null, "1"), React.createElement("li", null, "2"), React.createElement("li", null, "3")));
+  }, "test12355", React.createElement("h1", null, "tes heading"), React.createElement("ul", null, React.createElement("li", null, "1"), React.createElement("li", null, "2"), React.createElement("li", null, "3")), "p Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores porro suscipit quaerat pariatur adipisci amet nostrum culpa consequuntur, possimus doloribus laborum dolorem quos voluptatem ducimus molestiae modi perferendis aspernatur magni.");
 }
 export default App2;
