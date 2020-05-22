97080205743f9505015604618bf43fca3f632b6b
Shimon Wosner <SWosner@OKGTech.Local>
2020-05-22 11:43:11 -0400

    a

--- build/_dist_/App.js
+++
@@ -1,9 +0,0 @@
-import React from "/web_modules/react.js";
-import logo2 from "./logo.svg";
-import "./App.css";
-function App2() {
-  return React.createElement("div", {
-    className: "App"
-  }, "test");
-}
-export default App2;
