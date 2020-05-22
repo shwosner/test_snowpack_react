c5173a539c4db04e8c4ffb883e6e443d30a5b34a
Shimon Wosner <SWosner@OKGTech.Local>
2020-05-22 12:06:11 -0400

    build

---
+++ _dist_/App.js
@@ -0,0 +1,8 @@
+import React from "/web_modules/react.js";
+import "./App.css";
+function App2() {
+  return React.createElement("div", {
+    className: "App"
+  }, "test12355", React.createElement("h1", null, "tes heading"));
+}
+export default App2;
