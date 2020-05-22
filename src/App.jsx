5b33912398737cf436b5889e03d05c2edfd858a4
Shimon Wosner <SWosner@OKGTech.Local>
2020-05-22 11:46:35 -0400

    s

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className={'App'}>test12355</div>;
+  return (
+    <div className={'App'}>
+      test12355
+      <h1>tes heading</h1>
+    </div>
+  );
 }
 
 export default App;
